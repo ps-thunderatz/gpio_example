e6c405c8af922fa80300815898d05afda1488f31
Lucas Schneider <lucastrschneider@gmail.com>
2020-03-21 19:37:42 -0300

    Update source code, cube and config files

--- src/main.c
+++ src/main.c
@@ -6,21 +6,25 @@
 
 #include "mcu.h"
 
-/*****************************************
- * Private Constant Definitions
- *****************************************/
-
-#define LED_TOGGLE_DELAY_MS 1500
+#include "gpio.h"
 
 /*****************************************
  * Main Function
  *****************************************/
 
 int main(void) {
+    /* Inicialização do uC */
     mcu_init();
 
+    /* Loop principal */
     for (;;) {
-        led_toggle();
-        mcu_sleep(LED_TOGGLE_DELAY_MS);
+        /* Se o botão estiver apertado nesse instante */
+        if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET) {
+            /* Ligue o LED */
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+        } else {
+            /* Desligue o LED */
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        }
     }
 }
